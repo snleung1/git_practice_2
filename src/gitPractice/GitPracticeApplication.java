64b5b5b499d59f152a8edddc24ee559edb027481
snleung1 <snleung1@github.com>
2018-12-06 17:41:02 +0800

    master commit 2

--- src/gitPractice/GitPracticeApplication.java
+++ src/gitPractice/GitPracticeApplication.java
@@ -6,6 +6,6 @@ public class GitPracticeApplication {
 
 public static void main(String[] args) {
 		
-		System.out.println("Git Practice Main Branch checkin master 1");
+		System.out.println("Git Practice Main Branch checkin master 123");
 	}
 }
