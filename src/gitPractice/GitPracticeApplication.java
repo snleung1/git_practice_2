0f15036bfba7e228915cbcabbee821f967e53466
snleung1 <snleung1@github.com>
2018-12-06 17:57:29 +0800

    local checkin 4

--- src/gitPractice/GitPracticeApplication.java
+++ src/gitPractice/GitPracticeApplication.java
@@ -6,8 +6,10 @@ public class GitPracticeApplication {
 
 public static void main(String[] args) {
 		
+
 		System.out.println("Git Practice Main Branch checkin master 123 master 456");
 		System.out.println("Git Practice Main Branch checkin master 123");
+		System.out.println("Git Practice Main Branch checkin master 123 local change");
 		System.out.println("Git Practice Main Branch local branch 1");
 	}
 }
